f5dc1d81eacb86536cd7cd9f2747fbdc6e721b49
unknown <vitaliy.zdrok@gmail.com>
2018-08-27 00:10:10 +0300

    Emit newMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -13,12 +13,15 @@ const io = socketIO(server);
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) =>{
-	console.log('New user connected')
- 
-	socket.emit('newMessage', {
-		from: 'John',
-		text: 'See you then',
-		createdAt: 123123
+	console.log('New user connected') 
+
+	socket.on('createMessage', (message) => {
+		console.log('createMessage', message);
+		io.emit('newMessage', {
+			from: message.from,
+			text: message.text,
+			createdAt: new Date().getTime()
+		})
 	})
 
 });
